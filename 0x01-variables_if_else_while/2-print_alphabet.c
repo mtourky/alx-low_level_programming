a811efed8be9670c8ed7a439031010a258a5944e
mtourky <tourkym5@gmail.com>
2023-03-25 13:20:15 +0000

    from a to z in c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Discription:
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	for (char = 'a', char <= 'z', char++)
+{
+	putchar(char);
+}
+	putchar('\n');
+	return (0);
+}
