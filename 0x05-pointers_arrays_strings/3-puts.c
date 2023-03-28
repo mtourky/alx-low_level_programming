b153007f7a8aa70228417bfead0298f3022fe106
mtourky <tourkym5@gmail.com>
2023-03-28 00:19:31 +0000

    putchar

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - print string, followed by new line
+ *
+ * @str: print string param
+ *
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
