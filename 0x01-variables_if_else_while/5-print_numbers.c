5e191a6634cc5239af7d749fd295800885296dae
mtourky <tourkym5@gmail.com>
2023-09-09 16:27:18 +0300

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print digit number from 0 to 10
+ * Return:0
+ **/
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
