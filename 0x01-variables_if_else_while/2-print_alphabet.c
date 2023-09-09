d43c2d5bcf1acc0966862853f0721e3cb2e1a55f
mtourky <tourkym5@gmail.com>
2023-09-09 15:46:39 +0300

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print chars from a to z
+ * Return:0
+ **/
+int main(void)
+{
+	char x = 'a';
+
+	while (x < 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
