a6416b6c089e02e9942c740cc04496414bcd062b
mtourky <tourkym5@gmail.com>
2023-09-15 16:37:47 +0300

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* Description: print_sign - print + , 0 , - depend on values
+* @c: single character
+* Return: 1 , 0 , -1
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
